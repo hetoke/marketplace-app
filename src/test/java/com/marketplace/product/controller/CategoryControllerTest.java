bfd4a6dc3b01c2e2426146423fdcc8900df267d3
hetoke <hetokere32@gmail.com>
2026-06-13 16:06:25 +0700

    feat: restrict category write endpoints to ADMIN role only
    - POST/PUT/DELETE on /api/v1/categories/** now requires ROLE_ADMIN
    - GET remains public for all users
    - Cleaned up unused imports in CategoryControllerTest

--- src/test/java/com/marketplace/product/controller/CategoryControllerTest.java
+++ src/test/java/com/marketplace/product/controller/CategoryControllerTest.java
@@ -1,7 +1,6 @@
 package com.marketplace.product.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
