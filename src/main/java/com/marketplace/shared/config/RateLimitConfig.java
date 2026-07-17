190fb810d58b8a6be4886cb623ed4fa368cfab62
hetoke <hetokere32@gmail.com>
2026-07-18 05:35:22 +0700

    refactor: reuse Redis connection factory in RateLimitConfig

--- src/main/java/com/marketplace/shared/config/RateLimitConfig.java
+++ src/main/java/com/marketplace/shared/config/RateLimitConfig.java
@@ -1,8 +1,12 @@
 package com.marketplace.shared.config;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.AbstractRedisClient;
+import io.lettuce.core.cluster.RedisClusterClient;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
@@ -13,8 +17,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.BucketConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 @ConditionalOnProperty(prefix = "app.rate-limit", name = "enabled", havingValue = "true", matchIfMissing = true)
@@ -23,13 +26,29 @@ public class RateLimitConfig {
 
 	@Bean
 	public ProxyManager<String> proxyManager(RedisConnectionFactory connectionFactory) {
-		var redisClient = io.lettuce.core.RedisClient.create();
-		var connection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+		if (!(connectionFactory instanceof LettuceConnectionFactory lettuce)) {
+			throw new IllegalStateException("Bucket4j requires LettuceConnectionFactory");
+		}
 
-		return LettuceBasedProxyManager.builderFor(connection)
-				.withExpirationStrategy(
-						ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(10)))
-				.build();
+		AbstractRedisClient nativeClient = lettuce.getNativeClient();
+		RedisCodec<String, byte[]> codec = RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE);
+
+		return switch (nativeClient) {
+			case io.lettuce.core.RedisClient client -> LettuceBasedProxyManager
+					.builderFor(client.connect(codec))
+					.withExpirationStrategy(
+							ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
+									Duration.ofMinutes(10)))
+					.build();
+			case RedisClusterClient client -> LettuceBasedProxyManager
+					.builderFor(client.connect(codec))
+					.withExpirationStrategy(
+							ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
+									Duration.ofMinutes(10)))
+					.build();
+			default -> throw new IllegalStateException(
+					"Unsupported Redis client: " + nativeClient.getClass());
+		};
 	}
 
 	@Bean
